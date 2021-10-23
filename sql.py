203d68f316869c4d22b1e26219260d03422f4b13
Maksym-Leshchuk <maksym.leshchuk.knm.2020@lpnu.ua>
2021-10-23 12:56:20 +0300

    i

--- sql.py
+++ sql.py
@@ -1,17 +1,44 @@
+import os
+
 import app
 import pymysql
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, ForeignKey, String, BigInteger, DateTime, BINARY, func
+import sys
+
+# app.config["Secret"] = "Secret"
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password@localhost/db?host=localhost?port=3306"
 
 
+# SessionFactory = sessionmaker(bind=engine)
+#
+# Session = scoped_session(SessionFactory)
+#
+# BaseModel = declarative_base()
+#
+#
+#
+#
+# class user(BaseModel):
+#     __tablename__ = "user"
+#
+#     user_id = Column(Integer, primary_key=True)
+#     username = Column(String)
+#     first_name = Column(String)
+#     last_name = Column(String)
+#     email = Column(String)
+#     password = Column(String)
+#     phone = Column(String)
+#     user_status = Column(Integer)
+#
+#
+# class note(BaseModel):
+#     note_id = Column(Integer, primary_key=True)
+#     user_id = Column(Integer, primary_key=True)
+#     name = Column(String)
 
-app.config["Secret"] = "Secret"
-
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:password@localhost/db?host=localhost?port=3306"
-# engine = create_engine("mysql+pymysql://root:password@127.0.0.1/db?host=localhost?port=3306")
-# engine.connect()
-
-
-
-db = SQLAlchemy(app)
 
