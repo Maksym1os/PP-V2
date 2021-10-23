dace3f8f417911178ce52dc503cc844369398d3c
Maksym-Leshchuk <maksym.leshchuk.knm.2020@lpnu.ua>
2021-10-23 12:55:16 +0300

    i

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 from flask import Flask
+import os
 
+import app
+import pymysql
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, ForeignKey, String, BigInteger, DateTime, BINARY, func
+import sys
 
 app = Flask(__name__)
+
+engine = create_engine("mysql+pymysql://root:password@127.0.0.1/SimpleNotes")
+
+engine.connect();
 
 
 
