aee675044ae59ff01077a77a3d8607794d10fbf1
Maksym1os <maksym.los.knm.2020.@edu.lpnu.ua>
2021-11-04 18:07:24 +0200

    update 1.1

--- blueprint.py
+++ blueprint.py
@@ -11,32 +11,34 @@ from flask import request, jsonify, Response
 from functools import wraps
 
 
+session = Session()
+
+
 def db_lifecycle(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        with Session() as s:
-            try:
-                rez = func(*args, session=s, **kwargs)
-                s.commit()
-                return rez
-            except Exception as e:
-                if isinstance(e, ValueError):
-                    return jsonify({'message': e.args[0], 'type': 'ValueError'}), 400
-                elif isinstance(e, AttributeError):
-                    return jsonify({'message': e.args[0], 'type': 'AttributeError'}), 400
-                elif isinstance(e, KeyError):
-                    return jsonify({'message': e.args[0], 'type': 'KeyError'}), 400
-                elif isinstance(e, TypeError):
-                    return jsonify({'message': e.args[0], 'type': 'TypeError'}), 400
-                else:
-                    return jsonify({'message': str(e), 'type': 'InternalServerError'}), 500
+        try:
+            rez = func(*args, **kwargs)
+            session.commit()
+            return rez
+        except Exception as e:
+            if isinstance(e, ValueError):
+                return jsonify({'message': e.args[0], 'type': 'ValueError'}), 400
+            elif isinstance(e, AttributeError):
+                return jsonify({'message': e.args[0], 'type': 'AttributeError'}), 400
+            elif isinstance(e, KeyError):
+                return jsonify({'message': e.args[0], 'type': 'KeyError'}), 400
+            elif isinstance(e, TypeError):
+                return jsonify({'message': e.args[0], 'type': 'TypeError'}), 400
+            else:
+                return jsonify({'message': str(e), 'type': 'InternalServerError'}), 500
 
     return wrapper
 
 
 @app.route("/user", methods=["POST"])
 @db_lifecycle
-def create_user(session):
+def create_user():
     user_data = UserSchema().load(request.get_json())
     user_obj = db_utils.create_entry(user, **user_data)
 
@@ -48,23 +50,29 @@ def create_user(session):
 
 @app.route('/user', methods=["GET"])
 @db_lifecycle
-def get_users(session):
+def get_users():
     users = user.query.all()
     return jsonify(UserSchema(many=True).dump(users))
 
 
 @app.route("/user/<int:user_id>", methods=["GET"])
 @db_lifecycle
-def get_user_by_Id(user_id, session):
+def get_user_by_Id(user_id):
     user_obj = db_utils.get_entry_by_uid(user, user_id)
     return jsonify(UserSchema().dump(user_obj))
 
 
+# @app.route("/user/<string:username>", methods=["GET"])
+# def get_user_by_Id(username):
+#     user_obj = db_utils.get_entry_by_username(user, username)
+#     return jsonify(UserSchema().dump(user_obj))
+
+
 @app.route("/user/<int:user_id>", methods=["PUT"])
 @db_lifecycle
-def upd_user_by_Id(user_id, session):
+def upd_user_by_Id(user_id):
     new_data = UserSchema().load(request.get_json())
-    db_utils.update_entry_by_uid(session.query(user).filter_by(id=user_id).first(), **new_data)
+    db_utils.update_entry_by_uid(user, session, user_id, **new_data)
 
     return Response("", status=201, mimetype='application/json')
 
--- db_utils.py
+++ db_utils.py
@@ -6,6 +6,16 @@ def get_entry_by_uid(model_class, obj_id):
     return model_class.query.get(obj_id)
 
 
-def update_entry_by_uid(self, **kwargs):
-    for key, value in kwargs.items():
-        setattr(self, key, value)
+def get_entry_by_username(model, username):
+    return model.query.filter_by(username=username).first()
+
+
+def update_entry_by_uid(model, session, obj_id, **kwargs):
+
+    def update_model(self, **kwargs2):
+        for key, value in kwargs2.items():
+            setattr(self, key, value)
+
+    update_model(session.query(model).filter_by(id=obj_id).first(), **kwargs)
+
+
