70fa21fd0fce51c3bf02b554a7665d40ca12806a
Maksym1os <maksym.los.knm.2020.@edu.lpnu.ua>
2021-11-07 18:20:30 +0200

    serialization 1.2

--- database/blueprints/blpr_note.py
+++ database/blueprints/blpr_note.py
@@ -1,8 +1,32 @@
 from database.flask_ini import app
 
-from database.models import user
+from database.models import note
 
 from database.db_utils import *
 
-from database.schemas import UserSchema
+from database.schemas import NoteSchema
 
+
+@app.route("/note", methods=["POST"])
+def create_note():
+    return create_obj(NoteSchema, note)
+
+
+@app.route('/note', methods=["GET"])
+def get_notes():
+    return get_objects(NoteSchema, note)
+
+
+@app.route("/user/<int:Id>", methods=["GET"])
+def get_note_by_Id(Id):
+    return get_obj_by_Id(NoteSchema, note, Id)
+
+
+@app.route("/user/<int:Id>", methods=["PUT"])
+def upd_note_by_Id(Id):
+    return upd_obj_by_Id(NoteSchema, note, Id)
+
+
+@app.route("/user/<int:Id>", methods=["DELETE"])
+def delete_note_by_id(Id):
+    return delete_obj_by_id(NoteSchema, note, Id)
---
+++ database/blueprints/blpr_notelog.py
@@ -0,0 +1,27 @@
+from database.flask_ini import app
+
+from database.models import note_log
+
+from database.db_utils import *
+
+from database.schemas import NoteLogSchema
+
+
+@app.route("/note", methods=["POST"])
+def create_log():
+    return create_obj(NoteLogSchema, note_log)
+
+
+@app.route('/note', methods=["GET"])
+def get_logs():
+    return get_objects(NoteLogSchema, note_log)
+
+
+@app.route("/user/<int:Id>", methods=["GET"])
+def get_log_by_Id(Id):
+    return get_obj_by_Id(NoteLogSchema, note_log, Id)
+
+
+@app.route("/user/<int:Id>", methods=["PUT"])
+def upd_log_by_Id(Id):
+    return upd_obj_by_Id(NoteLogSchema, note_log, Id)
